d19d15ce682224db79d1145d1d2f13103520e923
John Wells <molekewl2017@gmail.com>
2020-08-05 13:22:23 -0700

    loader spin

--- script.js
+++ script.js
@@ -3,9 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
 
 // Quote API
 async function getQuote() {
+  loading();
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
@@ -27,7 +44,9 @@ async function getQuote() {
     }
 
     quoteText.innerText = data.quoteText;
-    console.log(data);
+    
+    // Stop loader & show quote
+    complete();
   } catch (error) {
     getQuote();
   }
