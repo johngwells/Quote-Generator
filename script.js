77cbf3658409ec11927eaf5a4af6631e133ea28f
John Wells <molekewl2017@gmail.com>
2020-08-05 13:01:24 -0700

    display quotes and check if quote is long to reduce font size & check if author is unknown

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // Quote API
 async function getQuote() {
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
@@ -5,10 +11,25 @@ async function getQuote() {
   try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
+
+    // Check if author name is not known
+    if (data.quoteAuthor === '') {
+      authorText.innerText = 'Unknown'
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+
+    // Reduce font size for long quotes
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
+    
+    quoteText.innerText = data.quoteText;
     console.log(data);
   } catch (error) {
     getQuote();
-    console.log('No quote available', error);
   }
 }
 
