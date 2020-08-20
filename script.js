f1c33226253e8f9af6e2b6b76c248f401bf025bb
John Wells <molekewl2017@gmail.com>
2020-08-20 15:55:31 -0700

    update

--- script.js
+++ script.js
@@ -6,14 +6,12 @@ const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
 
-// Show Loading
-function loading() {
+function showLoadingSpinner() {
   loader.hidden = false;
   quoteContainer.hidden = true;
 }
 
-// Hide Loading
-function complete() {
+function removeLoadingSpinner() {
   if (!loader.hidden) {
     quoteContainer.hidden = false;
     loader.hidden = true;
@@ -22,7 +20,7 @@ function complete() {
 
 // Quote API
 async function getQuote() {
-  loading();
+  showLoadingSpinner();
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
@@ -44,9 +42,7 @@ async function getQuote() {
     }
 
     quoteText.innerText = data.quoteText;
-    
-    // Stop loader & show quote
-    complete();
+    removeLoadingSpinner();
   } catch (error) {
     getQuote();
   }
